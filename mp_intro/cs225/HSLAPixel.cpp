d05596ee9fa5b5e0f9945461ddbda4261d794d11
Angelina <boynton3@illinois.edu>
2020-01-30 16:04:15 -0600

    Thinking outside the image

--- mp_intro/cs225/HSLAPixel.cpp
+++ mp_intro/cs225/HSLAPixel.cpp
@@ -6,6 +6,28 @@
  */
 
 #include "HSLAPixel.h"
+#include <cmath>
+#include <iostream>
+using namespace std;
 
 namespace cs225 {
+    HSLAPixel::HSLAPixel() {
+        h = 0;
+        s = 0;
+        l = 1;
+        a = 1;
+    }
+
+    HSLAPixel::HSLAPixel(double hue, double saturation, double luminance) {
+        h = hue;
+        s = saturation;
+        l = luminance;
+        a = 1;
+    }
+    HSLAPixel::HSLAPixel(double hue, double saturation, double luminance, double alpha) {
+        h = hue;
+        s = saturation;
+        l = luminance;
+        a = alpha;
+    }
 }
--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -3,9 +3,32 @@
 
 #include <string>
 
+using namespace cs225;
 
 void rotate(std::string inputFile, std::string outputFile) {
-  // TODO: Part 2
+  //read file
+  PNG original;
+  original.readFromFile(inputFile);
+  //set the size of the image
+  unsigned w = original.width();
+  unsigned h = original.height();
+
+  //need an output image
+  PNG output(w,h);
+
+//keep in mind, the image is starting from upper left corner
+//to rotate 180, have to run through height before width, otherwise it rotates by 90 ( I think )
+  for (unsigned y = 0; y < original.height(); y++) {
+    for (unsigned x = 0; x < original.width(); x++) {
+      //get current pixel 
+      HSLAPixel& pixel = original.getPixel(x,y);
+      //shift each pixel 
+      HSLAPixel& rotate = output.getPixel(w - x - 1, h - y);
+      rotate = pixel;
+    }
+  }
+  //write it back to the output
+  output.writeToFile(outputFile);
 }
 
 cs225::PNG myArt(unsigned int width, unsigned int height) {
